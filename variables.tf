cacd7e15295c52c441ed381225f37e3f7a7bdfc0
Phumthawan Nokun <knzphumthawan@gmail.com>
2024-10-21 15:53:13 +0700

    change for my variable

--- variables.tf
+++ variables.tf
@@ -1,13 +1,15 @@
 # Define variables for reusability and easy configuration
 variable "aws_region" {
-  default = "ca-central-1"
+  default = "ap-southeast-1"
 }
 
 variable "bucket_name" {
-  default = "my-static-website-bucket-67543"
+  description = "This is bucket name for s3"
+  default = "mersock-hd-static-website-bucket-67543"
 }
 
 variable "website_index_document" {
+  description = "This is websites index document"
   default = "index.html"
 }
 
